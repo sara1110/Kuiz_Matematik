b44a59ed0571b0a36319f43c307cca5c17c46d29
SaRa <65845923+sara1110@users.noreply.github.com>
2021-05-14 19:07:14 +0800

    murid_kemaskini.php folder complete with fixed bugs (hopefully

--- guru/murid_kemaskini.php
+++ guru/murid_kemaskini.php
@@ -57,3 +57,86 @@ if (!empty($_GET)){
 	}
 }
 ?>
+
+<!-- Bahagian untuk memaparkan senarai murid -->
+<h3>Senarai Murid</h3>
+
+<!-- link untuk memuat naik fail data murid -->
+<a href = 'murid_upload.php'>[+] Upload Data Murid</a>
+
+<!-- link untuk membesarkan saiz tulisan bagi aspek kepelbagaian pengguna -->
+<?PHP include ('../butang_saiz.php'); ?>
+
+<table width = "100%" border = "1" id = "besar">
+	<tr>
+		<td>Nama Murid</td>
+		<td>Id Murid</td>
+		<td>Katalaluan Murid</td>
+		<td>Kelas</td>
+		<td>Tindakan</td>
+	</tr>
+
+	<tr>
+		<!-- borang untuk mendaftar murid baharu -->
+		<form action = " " method = "POST">
+		<td><input type = "text" name = "nama_baru" value = "<?PHP echo $_GET['nama_pelajar']; ?>"></td>
+		<td><input type = "text" name = "id_baru" value = "<?PHP echo $_GET['id_pelajar'];?>"></td>
+		<td><input type = "password" name = "katalaluan_baru" value = "<?PHP echo $_GET['katalaluan_pelajar']; ?>"></td>
+		<td>
+			<select name = "id_kelas">
+				<option value selected disabled>Pilih</option>
+				<?PHP 
+				//arahan untuk mencari semua data murid yang berdaftar
+				$sql = "select * from KELAS";
+
+				//melaksanakan arahan mencari data
+				$laksana_arahan_cari = mysqli_query ($condb , $sql);
+
+				//pembolehubah $rekod_bilik mengambil data yang ditemui baris demi baris 
+				while ($rekod_bilik = mysqli_fetch_array($laksana_arahan_cari)) {
+
+					//memaparkan data yang ditemui dalam element 
+					echo "<option value = ".$rekod_bilik['tingkatan'].">".$rekod_bilik['nama_pelajar']."</option>";
+				}
+
+				?>
+				
+			</select>
+		</td>
+		<td><input type = "submit" value = "simpan"></td>
+	</tr>
+
+	<?PHP 
+	//arahan untuk mencari semua data murid yang berdaftar
+	$arahan_cari_murid = "select * from PELAJAR , KELAS 
+	where 
+	PELAJAR.id_kelas = KELAS.id_kelas
+	order by KELAS.tingkatan , KELAS.nama_kelas , PELAJAR.nama_pelajar ASC";
+
+	//melaksanakan arahan untuk mencari 
+	$laksana_cari_murid = mysqli_query ($condb , $arahan_cari_murid);
+
+	//pembolehubah $data mengambil semua data yang ditemui 
+	while ($data = mysqli_fetch_array ($laksana_cari_murid)) {
+
+		//mengumpukan data murid kedalam tatasusunan data_murid
+		$data_murid = array (
+
+			'nama_pelajar' => $data['nama_pelajar'],
+			'id_pelajar' => $data['id_pelajar'],
+			'katalaluan_pelajar' => $data['katalaluan_pelajar']
+		);
+
+		//memaparkan data murid baris demi baris 
+		echo "<tr>
+		<td>".$data['nama_pelajar']."</td>
+		<td>".$data['id_pelajar']."</td>
+		<td>".$data['katalaluan_pelajar']."</td>
+		<td>
+| <a href = 'murid_kemaskini.php?".http_build_query($data_murid)."'>Kemaskini</a>
+| <a href = 'padam.php?jadual=PELAJAR&medan=id_pelajar&id=".$data['id_pelajar']."'>Padam</a> |
+</td></tr>";
+	}
+	?>
+</table>
+<?PHP include ('footer_guru.php'); ?>
