d4323bd2c69f0cf2279fe559fb17b6a86bc57082
SaRa <65845923+sara1110@users.noreply.github.com>
2021-05-24 16:48:32 +0800

    completed soalan_set.php

--- guru/soalan_set.php
+++ guru/soalan_set.php
@@ -45,3 +45,74 @@ if (!empty($_POST)) {
 	}
 }
 ?>
+
+<!-- bahagian untuk memaparkan senarai set soalan -->
+<h3>Senarai Set Soalan</h3>
+<?PHP include ('../butang_saiz.php'); ?>
+<table width = '100%' border = '1' id = 'besar'>
+	<tr>
+		<td>Topik</td>
+		<td>Arahan</td>
+		<td>Jenis</td>
+		<td>Tarikh</td>
+		<td>Masa</td>
+		<td></td>
+	</tr>
+	<tr>
+		<!-- bahagian borang untuk mendaftar set soalan yang baru -->
+		<form action = '' method = 'POST'>
+			<td><textarea name = 'topik' rows = "4" cols = "25"></textarea></td>
+			<td><textarea name = 'arahan' rows = "4" cols = "25"></textarea></td>
+			<td>
+				<select name = 'jenis'>
+					<option value selected disabled>Pilih</option>
+					<option value = 'Latihan'>Latihan</option>
+					<option value = 'Kuiz'>Kuiz</option>	
+				</select>
+			</td>
+			<td><input type = "date" name = "tarikh"></td>
+			<td><input type = "number" name = "masa"></td>
+			
+		</form>
+	</tr>
+
+<?PHP 
+//arahan untuk memilih data dari jadual set soalan 
+$arahan_set = "select * from set_soalan order by no_set DESC";
+
+//melaksanakan arahan untuk memilih data 
+$laksana_set = mysqli_query ($condb , $arahan_set);
+
+//pembolehubah $data mengambil data yang ditemui 
+while ($data = mysqli_fetch_array ($laksana_set)) {
+
+	//mengumpulkan data yang ditemui ke dalam tatasusunan $data_get
+	$data_get = array (
+		'no_set' => $data['no_set'],
+		'topik' => $data['topik'],
+		'arahan' => $data['arahan'],
+		'jenis' => $data['jenis'],
+		'tarikh' => $data['tarikh'],
+		'masa' => $data['masa'], 
+		'id_guru' => $data['id_guru']
+	);
+
+//memaparkan data yang diambil baris demi baris 
+	echo "<tr>
+		<td>".$data['topik']."</td>
+		<td>".$data['arahan']."</td>
+		<td>".$data['jenis']."</td>
+		<td>".$data['tarikh']."</td>
+		<td>".$data['masa']."</td>
+		<td> 
+| <a href = 'soalan_daftar.php?no_set=".$data['no_set']."&topik=".$data['topik']."'>Soalan</a>
+| <a href = 'soalan_set_kemaskini.php?".http_build_query($data_get)."'>Kemaskini</a>
+
+| <a href = 'padam.php?jadual=set_soalan&medan=no_set&id=".$data['no_set']."' onClick=\"return confirm('Anda pasti anda ingin memadam data ini.')\">Padam</a> 
+		</td>
+	</tr>";
+}
+?>
+</table>
+
+<?PHP include ('footer_guru.php'); ?>
