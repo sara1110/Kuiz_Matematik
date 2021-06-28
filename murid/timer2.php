d4b5a85bb08eda4a1f59ea707c9648dae49c081e
SaRa <65845923+sara1110@users.noreply.github.com>
2021-06-28 17:46:40 +0800

    timer2.php file for murid fully coded

---
+++ murid/timer2.php
@@ -0,0 +1,40 @@
+<?PHP 
+function timer_kuiz ($masa) {
+
+	$jum_masa = $masa*60*1000;
+	echo"
+
+	<script type=\"text/javascript\">
+	var wait = setTimeout (\"document.soalan_kuiz.submit();\", ".$jum_masa.");
+	</script>
+	<div>Masa Menjawab: <span id=\"time\">".$masa.":00</span> Minit </div>
+
+	<script>
+	function startTimer(duration , display) {
+
+		var timer = duration , minutes , seconds;
+		setInterval(function () {
+			minutes = parseInt(timer / 60, 10);
+			seconds = parseInt(timer % 60, 10);
+
+			minutes = minutes <10 ? \"0\" + minutes : minutes;
+			seconds = <10 ? \"0\" + seconds : seconds;
+
+			display.textContent = minutes + \":\" + seconds;
+
+			if (--timer < 0) {
+
+				timer = duration;
+			}
+		}, 1000);
+	}
+
+	window.onload = function () {
+
+		var Minit = 60 * ".$masa.",
+			display = document.querySelector('#time');
+		startTimer(Minit , display);	
+	};
+</script>";
+}
+?>
