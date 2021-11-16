cc2847b0b5c4d0da138d69be1d594e47ebc3e6ed
SaRa <65845923+sara1110@users.noreply.github.com>
2021-11-16 09:22:37 +0800

    Officially done with project

--- guru/soalan_set_kemaskini.php
+++ guru/soalan_set_kemaskini.php
@@ -1,135 +1,133 @@
 <?PHP 
-//memanggil fail header_guru.php 
-include ('header_guru.php');
+# Memanggil header_guru.php
+include('header_guru.php'); 
 
-//bahagian untuk menyimpan data yang telah dikemakini 
+# ----- bahagian untuk menyimpan data set_soalan baru
 
-//menyemak kewujudan data GET 
-if (empty ($_GET)) {
+# Menyemak kewujudan data POST
+if(!empty($_POST))
+{
+    # Mengambil data POST
+    $topik  =   mysqli_real_escape_string($condb,$_POST['topik']);
+    $arahan =   mysqli_real_escape_string($condb,$_POST['arahan']);
+    $jenis  =   $_POST['jenis'];
+    $tarikh =   $_POST['tarikh'];
 
-	die("<script>alert('Akses tanpa kebenaran eh?? KELUARRRRR!!!!!!');
-		window.location.href = 'soalan_set.php?no_set=".$_GET['no_set']."&topik=".$_GET['topik']."';</script>");
-}
+    # Menetapkan masa kuiz
+    if($jenis=='Latihan')
+    $masa   =   "Tiada";
+    else
+    $masa   =   mysqli_real_escape_string($condb,$_POST['masa']);
+    
+    # menyemak kewujudan data yang diambil
+    if(empty($topik) or empty($arahan) or empty($jenis)or empty($tarikh)or empty($masa))
+    {
+        #jika terdapat pembolehubah yang tidak mempunyai nilai, aturcara akan dihentikan
+        die("<script>alert('Sila lengkapkan maklumat');
+        window.location.href='soalan_set.php';</script>");
+    }
 
-//menyemak kewujudan data POST 
-if (!empty ($_POST)) {
+    # Arahan untuk mengemaskini data set_soalan baru
+    $arahan_kemaskini=" update set_soalan set
+    topik	=	'$topik',
+    arahan	=	'$arahan',
+    jenis	=	'$jenis',
+    tarikh	=	'$tarikh',
+    masa	=	'$masa'
+    where
+    no_set	=	'".$_GET['no_set']."' ";
 
-	//mengambil data POST 
-	$soalan = mysqli_real_escape_string ($condb , $_POST['soalan']);
-	$jawapan_a = mysqli_real_escape_string ($condb , $_POST['jawapan_a']);
-	$jawapan_b = mysqli_real_escape_string ($condb , $_POST['jawapan_b']);
-	$jawapan_c = mysqli_real_escape_string ($condb , $_POST['jawapan_c']);
-	$jawapan_d = mysqli_real_escape_string ($condb , $_POST['jawapan_d']);
-
-	//menyemak kewujudan data yang telah diambil 
-	if (empty($soalan) or empty($jawapan_a) or empty($jawapan_b) or empty($jawapan_c) or empty($jawapan_d)) {
-
-		die("<script>alert('Sila lengkapkan maklumat');
-			window.history.back();</script");
-	}
-
-	//arahan mengemaskini soalan dan jawapan 
-	$arahan_kemaskini = "UPDATE soalan 
-	set 
-	soalan = '".$_POST['soalan']."',
-	jawapan_a = '".$_POST['jawapan_a']."',
-	jawapan_b = '".$_POST['jawapan_b']."',
-	jawapan_c = '".$_POST['jawapan_c']."',
-	jawapan_d = '".$_POST['jawapan_d']."'
-	where 
-	no_soalan = '".$_GET['no_soalan']."' ";
-
-	//melaksanakan arahan untuk mengemaskini soalan 
-	if (mysqli_query ($condb , $arahan_kemaskini)) {
-
-		//soalan berjaya disimpan 
-		//echo "<script>alert('Kemaskini BERJAYA YAYYYYYYYYY!!!!');
-		//window.location.href = 'soalan_set.php?no_set=".$_GET['no_set']."&topik=".$_GET['topik']."';</script>"; 
-	}
-
-	else { 
-
-		echo "<script>alert('Kemaskini GAGAL BOOOOOOO!!!!!!');
-		window.location.href = 'soalan_set.php?no_set=".$_GET['no_set']."&topik=".$_GET['topik']."';</script>";
-	}
-
+    if(mysqli_query($condb,$arahan_kemaskini))
+    {
+        # data berjaya diKemaskini
+        echo "<script>alert('Kemaskini BERJAYA.');
+        window.location.href='soalan_set.php';</script>";
+    }
+    else
+    {
+        # data gagal dikemaskini
+        echo "<script>alert('Kemaskini GAGAL.');
+        window.location.href='soalan_set.php';
+        </script>";
+    }
 }
 ?>
 
-<!-- Bahagian untuk memaparkan soalan yang telah didaftarkan -->
-<h3>Senarai Soalan</h3>
+<!-- bahagian untuk memaparkan senarai set soalan -->
+<h3>Senarai Set Soalan</h3>
 <?PHP include ('../butang_saiz.php'); ?>
-<table width = '100%' border = '1' id = 'besar'>
-	<tr>
-		<td>Soalan</td>
-		<td bgcolor = 'cyan'> Jawapan A (Betul)</td>
-		<td bgcolor = 'pink'>Jawapan B</td>
-		<td bgcolor = 'pink'>Jawapan C</td>
-		<td bgcolor = 'pink'>Jawapan D</td>
-		<td></td>
-	</tr>
-	<tr>
-	<!-- Bahagian borang untuk mengemaskini soalan dan jawapan -->
-<form action = '' method = 'POST'>
-
-<td><textarea name = 'soalan' rows = "4" cols = "25">
-<?PHP echo @$_GET['soalan']; ?></textarea></td>
-
-<td bgcolor = 'cyan'><textarea name = 'jawapan_a' rows = "4" cols = "25">
-<?PHP echo @$_GET['jawapan_a']; ?> </textarea></td>
-
-<td bgcolor = 'pink'><textarea name = 'jawapan_b' rows = "4" cols = "25">
-<?PHP echo @$_GET['jawapan_b']; ?> </textarea></td>
-
-<td bgcolor = 'pink'><textarea name = 'jawapan_c' rows = "4" cols = "25">
-<?PHP echo @$_GET['jawapan_c']; ?> </textarea></td>
-
-<td bgcolor = 'pink'><textarea name = 'jawapan_d' rows = "4" cols = "25">
-<?PHP echo @$_GET['jawapan_d']; ?> </textarea></td>
-
-<td><input type = 'submit' value = 'simpan'></td>
-
-</form>
-</tr>
-
-<?PHP 
-
-//arahan untuk mencari soalan yang berkaitan dengan set soalan yang telah dipilih 
-$arahan_soalan = "select * from soalan 
-where no_set = '".$_GET['no_set']."'
-order by no_soalan DESC";
-
-//melaksanakan arahan untuk mencari soalan 
-$laksana_soalan = mysqli_query ($condb , $arahan_soalan);
-
-//pembolehubah $data mengambil data yang ditemui 
-while ($data = mysqli_fetch_array ($laksana_soalan)) {
-
-	//mengumpukkan data yang ditemui kepada tatasusunan $data_get 
-	$data_get = array (
-		'no_set' => $data['no_set'],
-		'no_soalan' => $data['no_soalan'],
-		'soalan' => $data['soalan'],
-		'jawapan_a' => $data['jawapan_a'],
-		'jawapan_b' => $data['jawapan_b'],
-		'jawapan_c' => $data['jawapan_c'],
-		'jawapan_d' => $data['jawapan_d']
-	);
-
-	//memaparkan data yang ditemui baris demi baris 
-	"echo <tr>
-	<td>".$data['soalan']."</td>
-	<td>".$data['jawapan_a']."</td>
-	<td>".$data['jawapan_b']."</td>
-	<td>".$data['jawapan_c']."</td>
-	<td>".$data['jawapan_d']."</td>
+<table width='100%' border='1' id='besar'>
+    <tr>
+        <td>Topik</td>
+        <td>Arahan</td>
+        <td>Jenis</td>
+        <td>Tarikh</td>
+        <td>Masa</td>    
+        <td></td>   
+    </tr>
+    <tr>
+    <!-- bahagian borang untuk mendaftar set soalan yang baru -->
+<form action='' method='POST'>
 <td>
-| <a href = 'soalan_set_kemaskini.php?".http_build_query($data_get)."'>Kemaskini</a>
-
-| <a href = 'padam.php?jadual=soalan&medan=no_soalan&id=".$data['no_soalan']."'>Padam</a> |
+<textarea name='topik' rows="4" cols="25" ><?PHP echo $_GET['topik']; ?></textarea>
 </td>
-	</tr>";
+<td>
+<textarea name='arahan' rows="4" cols="25"><?PHP echo $_GET['arahan']; ?></textarea>
+</td>
+<td>
+<select name='jenis'>
+<option value='<?PHP echo $_GET['jenis']; ?>'><?PHP echo $_GET['jenis']; ?></option>
+<option value='Latihan'>Latihan</option>
+<option value='Kuiz'>Kuiz</option>                
+</select>
+</td>
+<td><input type='date' name='tarikh' value='<?PHP echo $_GET['tarikh']; ?>'></td>
+<td><input type='text' name='masa' value='<?PHP echo $_GET['masa']; ?>'></td>
+<td><input type='submit' value='Kemaskini'></td>
+        </form> 
+    </tr>
+<?PHP
+# arahan untuk memilih data dari jadual set soalan
+$arahan_set     =   "select* from set_soalan order by no_set DESC";
+
+# melaksanakan arahhan untuk memilih data
+$laksana_set    =   mysqli_query($condb,$arahan_set);
+
+# pembolehubah $data mengambil data yang ditemui
+while ($data=mysqli_fetch_array($laksana_set))
+{
+    # mengumpukkan data yang ditemui ke dalam tatasusunan $data_get
+    $data_get=array(
+        'no_set'    =>  $data['no_set'],
+        'topik'     =>  $data['topik'],
+        'arahan'    =>  $data['arahan'],
+        'jenis'     =>  $data['jenis'],
+        'tarikh'    =>  $data['tarikh'],
+        'masa'      =>  $data['masa'],
+        'id_guru' =>  $data['id_guru']
+    );
+ # Memaparkan data yang diambil baris demi baris
+ echo "<tr>
+ <td>    ".$data['topik']."  </td>
+ <td>    ".$data['arahan']." </td>
+ <td>    ".$data['jenis']."  </td>
+ <td>    ".$data['tarikh']." </td>
+ <td>    ".$data['masa']."   </td>
+ <td>
+
+|<a href='soalan_daftar.php?no_set=".$data['no_set']."&topik=".$data['topik']."'>Soalan</a>
+|<a href='soalan_set_kemaskini.php?".http_build_query($data_get)."'> Kemaskini </a>
+
+|<a href='padam.php?jadual=set_soalan&medan=no_set&id=".$data['no_set']."' 
+onClick=\"return confirm('Anda pasti anda ingin memadam data ini.')\" >Padam</a>|
+
+ </td> 
+</tr>";
 }
-?>	
+?>
 </table>
-<?PHP include ('footer_guru.php'); ?>
+<?PHP include('footer_guru.php'); ?>
+
+
+
+
