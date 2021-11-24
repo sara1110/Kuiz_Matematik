c6c43e17138786fd2c702b64c6309d8ab6b82ef5
SaRa <65845923+sara1110@users.noreply.github.com>
2021-11-24 11:31:21 +0800

    another error solved UGHHHHH

--- pelajar/jawab_semak.php
+++ pelajar/jawab_semak.php
@@ -1,39 +1,39 @@
-<?PHP
-include('../header.php');
-include('../connection.php');
-
+<?PHP 
+include ('../header.php');
 if(empty($_POST) and empty($_GET))
 {
     die("<script>alert('Akses tanpa kebenaran');
     window.location.href='pilih_latihan.php';</script>");
-}
+} 
+include ('../connection.php');
 $arahan_bil="select* from soalan where no_set='".$_GET['no_set']."'";
 $laksana_bil=mysqli_query($condb,$arahan_bil);
 $bil_soalan=mysqli_num_rows($laksana_bil);
-
 $betul=0;
 $salah=0;
 $bil=0;
 
-echo"<h3>Keputusan</h3>";
+echo"<h3>Keputusan Kuiz</h3>";
 echo"
 <table border='1' width='50%' id='besar'>
 <tr>
     <td>Bil</td>
     <td>soalan</td>
 </tr>";
-$arahan_simpan="insert into jawapan_pelajar(no_soalan,jawapan,catatan,id_pelajar)
+$arahan_simpan="insert into jawapan_pelajar 
+(no_soalan,jawapan,catatan,id_pelajar)
 values";
 foreach ($_POST as $key => $value)
-{
+{  
     $no_soalan=ltrim($key,"s");
-    $pecahkanbaris=explode("|",$value);
+    $pecahkanbaris = explode("|",$value);
     list($medan,$jawapan,$jawapan1,$jawapan2,$jawapan3,$jawapan4,$soalan,$jawapana,$gambar) = $pecahkanbaris;
-
+    
     if($gambar!=" ")
     {
         $gambar="<img src='".$gambar."'>";
-    }
+    }    
+    
 
     if($jawapan!="tidak jawab")
     {
@@ -44,6 +44,7 @@ foreach ($_POST as $key => $value)
         $nilai_jawapan='Tidak Jawab';
     }
 
+   # echo "Jawapan ".$no_soalan . " : ". $jawapan . "<br>"; 
     switch($medan)
     {
         case 'jawapan_a' : $betul++;break;
@@ -52,7 +53,7 @@ foreach ($_POST as $key => $value)
         case 'jawapan_d' : $salah++;break;
         default:$salah++; break;
     }
-
+    
     if($jawapan==$jawapana)
     {
         $warna="";
@@ -62,14 +63,16 @@ foreach ($_POST as $key => $value)
     {
         $warna="bgcolor='yellow'";
         $catatan="SALAH";
-        $medan='tidak jawab';
+        $medan='tidak jawap';
     }
     else
     {
         $warna="bgcolor='pink'";
         $catatan="SALAH";
     }
-    echo "<tr>
+    echo "
+    
+<tr>
     <td>".++$bil."</td>
     <td $warna>".$soalan."<br>$gambar.
     <br>";
@@ -80,17 +83,23 @@ foreach ($_POST as $key => $value)
             $tanda="checked='checked'";
         else
             $tanda="";
+        
+        
+
         echo"<input type='checkbox' name='$no_soalan' disabled='disabled' $tanda><label>".$$jawapans."</label><br>";
     }
+    
+      
     echo"</td>
-        </tr>
-        <tr>
-           
-            <td colspan='2' align='right'><b>Jawapan Pelajar :</b> $nilai_jawapan | <b>Jawapan Sebenar : </b>$jawapana</td>
-        </tr>";
-        $arahan_simpan=$arahan_simpan."('$no_soalan','$medan','$catatan','".$_SESSION['id_pelajar']."'),";
+    
+</tr>
+    <tr>
+        <td colspan='2' align='right'><b>Jawapan Pelajar :</b> $nilai_jawapan | <b>Jawapan Sebenar : </b>$jawapana</td>
+    </tr>";
+    $arahan_simpan=$arahan_simpan."('$no_soalan','$medan','$catatan','".$_SESSION['id_pelajar']."'),";
 }
 $arahan_simpan=rtrim($arahan_simpan,",");
+
 if(mysqli_query($condb,$arahan_simpan))
 {
     echo"<script>alert('Latihan tamat. data berjaya disimpan');</script>";
@@ -100,7 +109,9 @@ else
     echo"<script>alert('data gagal disimpan');
     window.history.back();
     </script>";
+    
 }
+
 echo "<hr>Jumlah markah : $betul / $bil_soalan";
 $peratus=($betul/$bil_soalan)*100;
 echo "<br>Peratus : ".number_format($peratus,2)." %<br>";
--- pelajar/timer2.php
+++ pelajar/timer2.php
@@ -1,40 +1,38 @@
-<?PHP 
-function timer_kuiz ($masa) {
+<?PHP
+function timer_kuiz($masa)
+{
+    $jum_masa=$masa*60*1000;
+echo"
 
-	$jum_masa = $masa*60*1000;
-	echo"
+<!-- Display the countdown timer in an element -->
+<script type=\"text/javascript\">
+var wait=setTimeout(\"document.soalan_kuiz.submit();\",".$jum_masa.");
+</script>
+<div>Masa menjawab : <span id=\"time\">".$masa.":00</span> Minit</div>
 
-	<script type=\"text/javascript\">
-	var wait = setTimeout (\"document.soalan_kuiz.submit();\", ".$jum_masa.");
-	</script>
-	<div>Masa Menjawab: <span id=\"time\">".$masa.":00</span> Minit </div>
+<script>
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
 
-	<script>
-	function startTimer(duration , display) {
+        minutes = minutes < 10 ? \"0\" + minutes : minutes;
+        seconds = seconds < 10 ? \"0\" + seconds : seconds;
 
-		var timer = duration , minutes , seconds;
-		setInterval(function () {
-			minutes = parseInt(timer / 60, 10);
-			seconds = parseInt(timer % 60, 10);
+        display.textContent = minutes + \":\" + seconds;
 
-			minutes = minutes <10 ? \"0\" + minutes : minutes;
-			seconds = <10 ? \"0\" + seconds : seconds;
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
 
-			display.textContent = minutes + \":\" + seconds;
-
-			if (--timer < 0) {
-
-				timer = duration;
-			}
-		}, 1000);
-	}
-
-	window.onload = function () {
-
-		var Minit = 60 * ".$masa.",
-			display = document.querySelector('#time');
-		startTimer(Minit , display);	
-	};
+window.onload = function () {
+    var Minit = 60 * ".$masa.",
+        display = document.querySelector('#time');
+    startTimer(Minit, display);
+};
 </script>";
 }
 ?>
